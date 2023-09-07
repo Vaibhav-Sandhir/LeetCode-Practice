b5e2a86b004bf913128d946703c8e720c78861d3
Vaibhav Sandhir <113308104+Vaibhav-Sandhir@users.noreply.github.com>
2023-09-07 23:25:10 +0530

    Queue Using Stacks Problem

---
+++ QueueUsingStacks.java
@@ -0,0 +1,39 @@
+class MyQueue {
+
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+
+    public MyQueue() {
+        
+    }
+    
+    public void push(int x) {
+        stack1.push(x);
+    }
+    
+    public int pop() {
+        if(!stack2.isEmpty())
+            return stack2.pop();
+        else{
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+            return stack2.pop();
+        }    
+    }
+    
+    public int peek() {
+        if(!stack2.isEmpty())
+            return stack2.peek();
+        else{
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+            return stack2.peek();
+        }   
+    }
+    
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
