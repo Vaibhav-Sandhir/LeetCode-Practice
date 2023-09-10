fc108a5fb09bb2963c1b61406cfb59d9330ae33e
Vaibhav Sandhir <113308104+Vaibhav-Sandhir@users.noreply.github.com>
2023-09-10 16:27:41 +0530

    Binary Tree Inorder Traversal Problem

---
+++ BinaryTreeInorderTraversal.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        inorder(root, list);
+        return list;
+    }
+
+    public void inorder(TreeNode node, List<Integer> list){
+        if(node == null)
+            return;
+
+        inorder(node.left, list);
+        list.add(node.val);
+        inorder(node.right, list);    
+    }
+}
