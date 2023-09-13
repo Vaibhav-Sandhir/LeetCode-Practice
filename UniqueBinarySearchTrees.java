0c257ebf05b53988b53c99ed234bd045eab5ccc4
Vaibhav Sandhir <113308104+Vaibhav-Sandhir@users.noreply.github.com>
2023-09-13 13:46:48 +0530

    Unique Binary Search Trees Problem

---
+++ UniqueBinarySearchTrees.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, 1);
+
+        for(int nodes = 2; nodes <= n; nodes++){
+            int total = 0;
+            for(int root = 1; root <= nodes; root++){
+                int left = root -1;
+                int right = nodes - root;
+                total += dp[left] * dp[right];
+            }
+            dp[nodes] = total;
+        }
+        return dp[n];
+    }
+}
