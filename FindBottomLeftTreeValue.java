b77e7dbb01031a31067bc7c5cb087c73b77c56f4
Vaibhav Sandhir <113308104+Vaibhav-Sandhir@users.noreply.github.com>
2024-01-04 22:43:20 +0530

    Find Bottom Left Tree Value Problem

---
+++ FindBottomLeftTreeValue.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
+
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            list = new ArrayList<>();
+            for(int i = 0; i < size; i++){
+                TreeNode node = queue.remove();
+                list.add(node.val);
+                if(node.left != null){
+                    queue.add(node.left);
+                }
+                if(node.right != null){
+                    queue.add(node.right);
+                }
+            }
+        }
+
+        return list.get(0);
+    }
+}
